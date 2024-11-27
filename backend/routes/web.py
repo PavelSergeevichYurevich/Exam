ebe0a17fab933bd4386bab84677b67d3d7a09da5
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-11-27 11:14:40 +0700

    users

--- backend/routes/web.py
+++ backend/routes/web.py
@@ -20,6 +20,18 @@ async def index(request: Request):
 async def login(request:Request):
     return templates.TemplateResponse(request=request, name="login.html")
 
+@web_router.get("/users/", response_class = HTMLResponse)
+async def get_users_page(request:Request, db: Session = Depends(get_db)):
+    stmnt = select(User)
+    users:list = db.scalars(stmnt).all()
+    context:dict = {}
+    i:int = 1
+    for user in users:
+        new_el = {str(i): user.username}
+        context.update(new_el)
+        i += 1
+    return templates.TemplateResponse("users.html", {"request": request, "context": context})
+
 @web_router.get("/register/")
 async def login(request:Request):
     return templates.TemplateResponse(request=request, name="register.html")
